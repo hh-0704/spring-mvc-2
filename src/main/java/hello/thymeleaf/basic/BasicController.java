b45c9cf9507d39d3c5148d4afb56e48d09b2de8b
hh-0704 <ghksl0704@naver.com>
2026-03-15 22:42:57 +0900

    2-18 자바스크립트 인라인

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -132,6 +132,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     @Data
     public static class User {
         private String username;
